59676fafa6b86a75bdf8d642cd3a866ccdfc6fe5
z0xyz <solicitmind@gmail.com>
2021-11-02 16:21:59 +0200

    new commit

--- file.js
+++ file.js
@@ -1,1 +1,9 @@
 console.log("The project got just initialized!")
+
+
+noteObject = {
+    1:"first note is that i'm really a cool type of person",
+    2: "Secodn note is that i'm not really into these type of stuff right now"
+}
+
+console.log(noteObject[1])
