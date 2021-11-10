6fc0c6db58b5ebf842bdf19652d46c53fdd3e74c
z0xyz <solicitmind@gmail.com>
2021-11-10 12:24:51 +0200

    Adding the ability to add notes , and next i'm going to implement note title and value along with other enhancements

--- file.js
+++ file.js
@@ -1,3 +1,5 @@
+#!/usr/bin/node
+
 const fs = require("fs")
 const yargs = require("yargs")
 
@@ -14,6 +16,8 @@ const argv = yargs
             type : "string"
         }
     })
+    //This add function should be more sophisticated later on with accompanying capability to check the current loged in user and whatnot
+    .command ("add [note]" , "Add a note to the first user") 
     .option('time', {
         description: 'Tell the present Time',
         alias: 't',
@@ -27,15 +31,11 @@ if (argv.time) {
     console.log('The current time is: ', new Date().toLocaleTimeString());
 }
 
-if (argv.note) {
-    console.log("The current note is " , argv.note)
-}
-
 let rawData = fs.readFileSync("./file.json")
-let student = JSON.parse(rawData)
+let parsedData = JSON.parse(rawData)
 
 //Check if an option is passed or not
-function optionAddCheck(option){
+function optionCheck(option){
     if (option != undefined) {
         return true
     }else {
@@ -45,10 +45,10 @@ function optionAddCheck(option){
 
 if (argv._.includes("list")){
     let userName = argv.user
-    if (optionAddCheck(userName)) {
-        if (student.hasOwnProperty(userName)) {
+    if (optionCheck(userName)) {
+        if (parsedData.hasOwnProperty(userName)) {
             console.log("The notes for " , userName)
-            let userNotes = student[userName]['notes']
+            let userNotes = parsedData[userName]['notes']
             for (let i = 0 ; i < userNotes.length ; i++){
                 console.log(userNotes[i])
             }
@@ -57,10 +57,32 @@ if (argv._.includes("list")){
         }
     }else {
         console.log("The list of users ")
-        let usersList = Object.keys(student)
+        let usersList = Object.keys(parsedData)
         for (let i = 0 ; i < usersList.length ; i++ ){
             console.log(usersList[i])
-            //console.log(student[usersList[i]]['name'])
+            //console.log(parsedData[usersList[i]]['name'])
         }
     }
 }
+
+if (argv._.includes("add")) {
+    if (argv.note != undefined) {
+        console.log(argv.note)
+    }else {
+        console.log("Please provide a valid note!")
+    }
+}
+
+
+//let newUser = {
+//    "name":"mostafa",
+//    "age":19,
+//    "hobbies":[
+//        "gaming",
+//        "coding",
+//        "doing math"
+//    ]
+//}
+//
+//let newUserData = JSON.stringify(newUser , null , 2)
+//fs.writeFileSync('newUserFile.json', newUserData)
